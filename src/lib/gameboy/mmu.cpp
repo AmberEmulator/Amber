cefd73e8350531f01d65b6903a212f350eb184db
Rick de Water <rick_water@hotmail.com>
2019-09-23 01:13:33 +0200

    Refractor MMU

--- src/lib/gameboy/mmu.cpp
+++ src/lib/gameboy/mmu.cpp
@@ -10,272 +10,320 @@ using namespace Amber;
 using namespace Common;
 using namespace Gameboy;
 
-void MMU::SetBootROM(ROM* a_BootROM)
+MMU::MMU()
+{
+	for (uint8_t i = 0; i < 0xF; ++i)
+	{
+		m_PageLoads[i] = &MMU::LoadNOP;
+		m_PageStores[i] = &MMU::StoreNOP;
+	}
+
+	m_PageLoads[0xF] = &MMU::LoadLastPage;
+	m_PageStores[0xF] = &MMU::StoreLastPage;
+
+	for (size_t i = 0; i < 512; ++i)
+	{
+		m_LastLoads[i] = &MMU::LoadNOP;
+		m_LastStores[i] = &MMU::StoreNOP;
+	}
+
+	for (size_t i = 0; i < 0xA0; ++i)
+	{
+		m_LastLoads[i] = &MMU::LoadArray<&MMU::m_OAM, 0xFE00>;
+		m_LastStores[i] = &MMU::StoreArray<&MMU::m_OAM, 0xFE00>;
+	}
+
+	for (size_t i = 0x180; i < 0x1FF; ++i)
+	{
+		m_LastLoads[i] = &MMU::LoadArray<&MMU::m_HRAM, 0xFF80>;
+		m_LastStores[i] = &MMU::StoreArray<&MMU::m_HRAM, 0xFF80>;
+	}
+}
+
+void MMU::SetBootROM(Memory* a_BootROM)
 {
 	m_BootROM = a_BootROM;
+	if (m_BootROM != nullptr)
+	{
+		m_PageLoads[0x0] = &MMU::LoadBoot;
+		m_PageStores[0x0] = &MMU::StoreBoot;
+
+		m_LastStores[0x0150] = &MMU::StoreDisableBoot;
+	}
+	else
+	{
+		if (m_Cartridge != nullptr)
+		{
+			m_PageLoads[0x0] = &MMU::LoadMemory<&MMU::m_Cartridge, 0>;
+			m_PageStores[0x0] = &MMU::StoreMemory<&MMU::m_Cartridge, 0>;
+		}
+		else
+		{
+			m_PageLoads[0x0] = &MMU::LoadNOP;
+			m_PageStores[0x0] = &MMU::StoreNOP;
+		}
+
+		m_LastStores[0x0150] = &MMU::StoreNOP;
+	}
 }
 
 void MMU::SetCartridge(Memory* a_Cartridge)
 {
 	m_Cartridge = a_Cartridge;
+	if (m_Cartridge != nullptr)
+	{
+		for (uint8_t i = 0x0; i < 0x8; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadMemory<&MMU::m_Cartridge, 0>;
+			m_PageStores[i] = &MMU::StoreMemory<&MMU::m_Cartridge, 0>;
+		}
+	}
+	else
+	{
+		for (uint8_t i = 0x0; i < 0x8; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadNOP;
+			m_PageStores[i] = &MMU::StoreNOP;
+		}
+	}
+
+	SetBootROM(m_BootROM);
 }
 
-void MMU::SetVRAM(RAM* a_VRAM)
+void MMU::SetVRAM(Memory* a_VRAM)
 {
 	m_VRAM = a_VRAM;
+	if (m_VRAM != nullptr)
+	{
+		for (uint8_t i = 0x8; i < 0xA; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadMemory<&MMU::m_VRAM, 0x8000>;
+			m_PageStores[i] = &MMU::StoreMemory<&MMU::m_VRAM, 0x8000>;
+		}
+	}
+	else
+	{
+		for (uint8_t i = 0x8; i < 0xA; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadNOP;
+			m_PageStores[i] = &MMU::StoreNOP;
+		}
+	}
 }
 
-void MMU::SetWRAM(RAM* a_WRAM)
+void MMU::SetWRAM(Memory* a_WRAM)
 {
 	m_WRAM = a_WRAM;
+	if (m_WRAM != nullptr)
+	{
+		for (uint8_t i = 0xC; i < 0xE; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadMemory<&MMU::m_WRAM, 0xC000>;
+			m_PageStores[i] = &MMU::StoreMemory<&MMU::m_WRAM, 0xC000>;
+		}
+
+		m_PageLoads[0xE] = &MMU::LoadMemory<&MMU::m_WRAM, 0xE000>;
+		m_PageStores[0xE] = &MMU::StoreMemory<&MMU::m_WRAM, 0xE000>;
+	}
+	else
+	{
+		for (uint8_t i = 0xC; i < 0xF; ++i)
+		{
+			m_PageLoads[i] = &MMU::LoadNOP;
+			m_PageStores[i] = &MMU::StoreNOP;
+		}
+	}
 }
 
 void MMU::SetCPU(CPU* a_CPU)
 {
 	m_CPU = a_CPU;
+	if (m_CPU != nullptr)
+	{
+		m_LastLoads[0x0104] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetDIV>;
+		m_LastLoads[0x0105] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetTIMA>;
+		m_LastLoads[0x0106] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetTMA>;
+		m_LastLoads[0x0107] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetTAC>;
+		m_LastLoads[0x010F] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetInterruptRequests>;
+		m_LastLoads[0x01FF] = &MMU::LoadRegister<&MMU::m_CPU, &CPU::GetInterruptEnable>;
+		
+		m_LastStores[0x0104] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetDIV>;
+		m_LastStores[0x0105] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetTIMA>;
+		m_LastStores[0x0106] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetTMA>;
+		m_LastStores[0x0107] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetTAC>;
+		m_LastStores[0x010F] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetInterruptRequests>;
+		m_LastStores[0x0146] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::StartDMA>;
+		m_LastStores[0x01FF] = &MMU::StoreRegister<&MMU::m_CPU, &CPU::SetInterruptEnable>;
+	}
+	else
+	{
+		m_LastLoads[0x0104] = &MMU::LoadNOP;
+		m_LastLoads[0x0105] = &MMU::LoadNOP;
+		m_LastLoads[0x0106] = &MMU::LoadNOP;
+		m_LastLoads[0x0107] = &MMU::LoadNOP;
+		m_LastLoads[0x010F] = &MMU::LoadNOP;
+		m_LastLoads[0x01FF] = &MMU::LoadNOP;
+
+		m_LastStores[0x0104] = &MMU::StoreNOP;
+		m_LastStores[0x0105] = &MMU::StoreNOP;
+		m_LastStores[0x0106] = &MMU::StoreNOP;
+		m_LastStores[0x0107] = &MMU::StoreNOP;
+		m_LastStores[0x010F] = &MMU::StoreNOP;
+		m_LastStores[0x0146] = &MMU::StoreNOP;
+		m_LastStores[0x01FF] = &MMU::StoreNOP;
+	}
 }
 
 void MMU::SetPPU(PPU* a_PPU)
 {
 	m_PPU = a_PPU;
+	if (m_PPU != nullptr)
+	{
+		m_LastLoads[0x0140] = &MMU::LoadRegister<&MMU::m_PPU, &PPU::GetLCDC>;
+		m_LastLoads[0x0142] = &MMU::LoadRegister<&MMU::m_PPU, &PPU::GetSCY>;
+		m_LastLoads[0x0143] = &MMU::LoadRegister<&MMU::m_PPU, &PPU::GetSCX>;
+		m_LastLoads[0x0144] = &MMU::LoadRegister<&MMU::m_PPU, &PPU::GetLY>;
+		
+		m_LastStores[0x0140] = &MMU::StoreRegister<&MMU::m_PPU, &PPU::SetLCDC>;
+		m_LastStores[0x0142] = &MMU::StoreRegister<&MMU::m_PPU, &PPU::SetSCY>;
+		m_LastStores[0x0143] = &MMU::StoreRegister<&MMU::m_PPU, &PPU::SetSCX>;
+	}
+	else
+	{
+		m_LastLoads[0x0140] = &MMU::LoadNOP;
+		m_LastLoads[0x0142] = &MMU::LoadNOP;
+		m_LastLoads[0x0143] = &MMU::LoadNOP;
+		m_LastLoads[0x0144] = &MMU::LoadNOP;
+		
+		m_LastStores[0x0140] = &MMU::StoreNOP;
+		m_LastStores[0x0142] = &MMU::StoreNOP;
+		m_LastStores[0x0143] = &MMU::StoreNOP;
+	}
 }
 
 void MMU::SetJoypad(Joypad* a_Joypad)
 {
 	m_Joypad = a_Joypad;
+	if (m_Joypad != nullptr)
+	{
+		m_LastLoads[0x0100] = &MMU::LoadRegister<&MMU::m_Joypad, &Joypad::GetRegister>;
+		m_LastStores[0x0100] = &MMU::StoreRegister<&MMU::m_Joypad, & Joypad::SetRegister>;
+	}
+	else
+	{
+		m_LastLoads[0x0100] = &MMU::LoadNOP;
+		m_LastStores[0x0100] = &MMU::StoreNOP;
+	}
 }
 
 uint8_t MMU::Load8(Address a_Address) const
 {
 	const uint16_t page = a_Address >> 12;
 
-	switch (page)
+	if (page == 8 || page == 9)
 	{
-		case 0x0:
-		if (a_Address <= 0xFF && m_BootROMEnabled && m_BootROM != nullptr)
-		{
-			return m_BootROM->Load8(a_Address);
-		}
-		case 0x1:
-		case 0x2:
-		case 0x3:
-		case 0x4:
-		case 0x5:
-		case 0x6:
-		case 0x7:
-		case 0xA:
-		case 0xB:
-		if (m_Cartridge != nullptr)
-		{
-			return m_Cartridge->Load8(a_Address);
-		}
-		break;
-
-		case 0x8:
-		case 0x9:
-		if (m_VRAM != nullptr)
-		{
-			return m_VRAM->Load8(a_Address - 0x8000);
-		}
-		break;
-		
-		case 0xC:
-		case 0xD:
-		if (m_WRAM != nullptr)
-		{
-			return m_WRAM->Load8(a_Address - 0xC000);
-		}
-		break;
-
-		case 0xE:
-		if (m_WRAM != nullptr)
-		{
-			return m_WRAM->Load8(a_Address - 0xE000);
-		}
-		break;
-
-		case 0xF:
-		if (a_Address <= 0xFDFF)
-		{
-			return m_WRAM->Load8(a_Address - 0xE000);
-		}
-		else if (a_Address >= 0xFE00 && a_Address <= 0xFE9F)
-		{
-			return m_OAM[a_Address - 0xFE00];
-		}
-		else if (a_Address >= 0xFF80 && a_Address <= 0xFFFE)
-		{
-			return m_HRAM[a_Address - 0xFF80];
-		}
-		else if (a_Address == 0xFF04 && m_CPU != nullptr)
-		{
-			return m_CPU->GetDIV();
-		}
-		else if (a_Address == 0xFF05 && m_CPU != nullptr)
-		{
-			return m_CPU->GetTIMA();
-		}
-		else if (a_Address == 0xFF06 && m_CPU != nullptr)
-		{
-			return m_CPU->GetTMA();
-		}
-		else if (a_Address == 0xFF07 && m_CPU != nullptr)
-		{
-			return m_CPU->GetTAC();
-		}
-		else if (a_Address == 0xFF0F && m_CPU != nullptr)
-		{
-			return m_CPU->GetInterruptRequests();
-		}
-		else if (a_Address == 0xFFFF && m_CPU != nullptr)
-		{
-			return m_CPU->GetInterruptEnable();
-		}
-		else if (a_Address == 0xFF00 && m_Joypad != nullptr)
-		{
-			return m_Joypad->GetRegister();
-		}
-		else if (a_Address == 0xFF40 && m_PPU != nullptr)
-		{
-			return m_PPU->GetLCDC();
-		}
-		else if (a_Address == 0xFF42 && m_PPU != nullptr)
-		{
-			return m_PPU->GetSCX();
-		}
-		else if (a_Address == 0xFF43 && m_PPU != nullptr)
-		{
-			return m_PPU->GetSCY();
-		}
-		else if (a_Address == 0xFF44 && m_PPU != nullptr)
-		{
-			return m_PPU->GetLY();
-		}
-		break;
+		bool boop = true;
 	}
 
-	return 0xFF;
+	return (this->*(m_PageLoads[page]))(a_Address);
 }
 
 void MMU::Store8(Address a_Address, uint8_t a_Value)
 {
 	const uint16_t page = a_Address >> 12;
-
-	switch (page)
-	{
-		case 0x0:
-		case 0x1:
-		case 0x2:
-		case 0x3:
-		case 0x4:
-		case 0x5:
-		case 0x6:
-		case 0x7:
-		case 0xA:
-		case 0xB:
-		if (m_Cartridge != nullptr)
-		{
-			m_Cartridge->Store8(a_Address, a_Value);
-		}
-		break;
-
-		case 0x8:
-		case 0x9:
-		if (m_VRAM != nullptr)
-		{
-			m_VRAM->Store8(a_Address - 0x8000, a_Value);
-		}
-		break;
-
-		case 0xC:
-		case 0xD:
-		if (m_WRAM != nullptr)
-		{
-			m_WRAM->Store8(a_Address - 0xC000, a_Value);
-		}
-		break;
-
-		case 0xE:
-		if (m_WRAM != nullptr)
-		{
-			m_WRAM->Load8(a_Address - 0xE000);
-		}
-		break;
-
-		case 0xF:
-		if (a_Address <= 0xFDFF)
-		{
-			m_WRAM->Store8(a_Address - 0xE000, a_Value);
-		}
-		else if (a_Address >= 0xFE00 && a_Address <= 0xFE9F)
-		{
-			m_OAM[a_Address - 0xFE00] = a_Value;
-		}
-		else if (a_Address >= 0xFF80 && a_Address <= 0xFFFE)
-		{
-			m_HRAM[a_Address - 0xFF80] = a_Value;
-		}
-		else if (a_Address == 0xFF46 && m_CPU != nullptr)
-		{
-			m_CPU->StartDMA(a_Value);
-		}
-		else if (a_Address == 0xFF50)
-		{
-			m_BootROMEnabled = false;
-		}
-		else if (a_Address == 0xFF04 && m_CPU != nullptr)
-		{
-			m_CPU->SetDIV(a_Value);
-		}
-		else if (a_Address == 0xFF05 && m_CPU != nullptr)
-		{
-			m_CPU->SetTIMA(a_Value);
-		}
-		else if (a_Address == 0xFF06 && m_CPU != nullptr)
-		{
-			m_CPU->SetTMA(a_Value);
-		}
-		else if (a_Address == 0xFF07 && m_CPU != nullptr)
-		{
-			m_CPU->SetTAC(a_Value);
-		}
-		else if (a_Address == 0xFF0F && m_CPU != nullptr)
-		{
-			m_CPU->SetInterruptRequests(a_Value);
-		}
-		else if (a_Address == 0xFFFF && m_CPU != nullptr)
-		{
-			m_CPU->SetInterruptEnable(a_Value);
-		}
-		else if (a_Address == 0xFF01)
-		{
-			m_TestCharacter = a_Value;
-		}
-		else if (a_Address == 0xFF02 && a_Value == 0x81)
-		{
-			std::cout << m_TestCharacter;
-		}
-		else if (a_Address == 0xFF00 && m_Joypad != nullptr)
-		{
-			m_Joypad->SetRegister(a_Value);
-		}
-		else if (a_Address == 0xFF40 && m_PPU != nullptr)
-		{
-			m_PPU->SetLCDC(a_Value);
-		}
-		else if (a_Address == 0xFF42 && m_PPU != nullptr)
-		{
-			m_PPU->SetSCX(a_Value);
-		}
-		else if (a_Address == 0xFF43 && m_PPU != nullptr)
-		{
-			m_PPU->SetSCY(a_Value);
-		}
-		break;
-	}
+	(this->*(m_PageStores[page]))(a_Address, a_Value);
 }
 
 void MMU::Reset()
 {
-	m_BootROMEnabled = true;
+	SetBootROM(m_BootROM);
+}
+
+uint8_t MMU::LoadNOP(uint16_t a_Address) const
+{
+	return 0xFF;
+}
+
+uint8_t MMU::LoadBoot(uint16_t a_Address) const
+{
+	if (a_Address <= 0xFF)
+	{
+		return LoadMemory<&MMU::m_BootROM, 0>(a_Address);
+	}
+	else if (m_Cartridge != nullptr)
+	{
+		return LoadMemory<&MMU::m_Cartridge, 0>(a_Address);
+	}
+	else
+	{
+		return LoadNOP(a_Address);
+	}
+}
+
+uint8_t MMU::LoadLastPage(uint16_t a_Address) const
+{
+	if ((a_Address & 0xFE00) != 0xFE00)
+	{
+		if (m_WRAM != nullptr)
+		{
+			return LoadMemory<&MMU::m_WRAM, 0xE000>(a_Address);
+		}
+		else
+		{
+			return LoadNOP(a_Address);
+		}
+	}
+	else
+	{
+		return (this->*(m_LastLoads[a_Address & 0x1FF]))(a_Address);
+	}
+}
+
+void MMU::StoreNOP(uint16_t a_Address, uint8_t a_Value)
+{
+}
+
+void MMU::StoreBoot(uint16_t a_Address, uint8_t a_Value)
+{
+	if (a_Address <= 0xFF)
+	{
+		StoreMemory<&MMU::m_BootROM, 0>(a_Address, a_Value);
+	}
+	else if (m_Cartridge != nullptr)
+	{
+		StoreMemory<&MMU::m_Cartridge, 0>(a_Address, a_Value);
+	}
+	else
+	{
+		StoreNOP(a_Address, a_Value);
+	}
+}
+
+void MMU::StoreDisableBoot(uint16_t a_Address, uint8_t a_Value)
+{
+	if (m_Cartridge != nullptr)
+	{
+		m_PageLoads[0x0] = &MMU::LoadMemory<&MMU::m_Cartridge, 0>;
+		m_PageStores[0x0] = &MMU::StoreMemory<&MMU::m_Cartridge, 0>;
+	}
+	else
+	{
+		m_PageLoads[0x0] = &MMU::LoadNOP;
+		m_PageStores[0x0] = &MMU::StoreNOP;
+	}
+}
+
+void MMU::StoreLastPage(uint16_t a_Address, uint8_t a_Value)
+{
+	if ((a_Address & 0xFE00) != 0xFE00)
+	{
+		if (m_WRAM != nullptr)
+		{
+			StoreMemory<&MMU::m_WRAM, 0xE000>(a_Address, a_Value);
+		}
+	}
+	else
+	{
+		(this->*(m_LastStores[a_Address & 0x1FF]))(a_Address, a_Value);
+	}
 }
--- src/lib/gameboy/mmu.hpp
+++ src/lib/gameboy/mmu.hpp
@@ -3,7 +3,7 @@
 
 #include <gameboy/api.hpp>
 
-#include <common/ram.hpp>
+#include <common/memory.hpp>
 
 namespace Amber::Gameboy
 {
@@ -14,13 +14,12 @@ namespace Amber::Gameboy
 	class GAMEBOY_API MMU : public Common::MemoryHelper<uint16_t, false>
 	{
 		public:
-		using ROM = Common::ROM16<false>;
-		using RAM = Common::RAM16<false>;
+		MMU();
 
-		void SetBootROM(ROM* a_BootROM);
+		void SetBootROM(Memory* a_BootROM);
 		void SetCartridge(Memory* a_Cartridge);
-		void SetVRAM(RAM* a_VRAM);
-		void SetWRAM(RAM* a_WRAM);
+		void SetVRAM(Memory* a_VRAM);
+		void SetWRAM(Memory* a_WRAM);
 		void SetCPU(CPU* a_CPU);
 		void SetPPU(PPU* a_PPU);
 		void SetJoypad(Joypad* a_Joypad);
@@ -31,18 +30,62 @@ namespace Amber::Gameboy
 		void Reset();
 
 		private:
-		ROM* m_BootROM = nullptr;
+		using LoadOp = uint8_t(MMU::*)(uint16_t a_Address) const;
+		using StoreOp = void (MMU::*)(uint16_t a_Address, uint8_t a_Value);
+
+		uint8_t LoadNOP(uint16_t a_Address) const;
+		uint8_t LoadBoot(uint16_t a_Address) const;
+		uint8_t LoadLastPage(uint16_t a_Address) const;
+		template <auto Member, uint16_t a_Offset>
+		uint8_t LoadMemory(uint16_t a_Address) const
+		{
+			return (this->*Member)->Load8(a_Address - a_Offset);
+		}
+		template <auto Member, uint16_t a_Offset>
+		uint8_t LoadArray(uint16_t a_Address) const
+		{
+			return (this->*Member)[a_Address - a_Offset];
+		}
+		template <auto Member, auto Op>
+		uint8_t LoadRegister(uint16_t a_Address) const
+		{
+			return ((this->*Member)->*Op)();
+		}
+
+		void StoreNOP(uint16_t a_Address, uint8_t a_Value);
+		void StoreBoot(uint16_t a_Address, uint8_t a_Value);
+		void StoreDisableBoot(uint16_t a_Address, uint8_t a_Value);
+		void StoreLastPage(uint16_t a_Address, uint8_t a_Value);
+		template <auto Member, uint16_t a_Offset>
+		void StoreMemory(uint16_t a_Address, uint8_t a_Value)
+		{
+			(this->*Member)->Store(a_Address - a_Offset, a_Value);
+		}
+		template <auto Member, uint16_t a_Offset>
+		void StoreArray(uint16_t a_Address, uint8_t a_Value)
+		{
+			(this->*Member)[a_Address - a_Offset] = a_Value;
+		}
+		template <auto Member, auto Op>
+		void StoreRegister(uint16_t a_Address, uint8_t a_Value)
+		{
+			((this->*Member)->*Op)(a_Value);
+		}
+
+		LoadOp m_PageLoads[16];
+		StoreOp m_PageStores[16];
+		LoadOp m_LastLoads[512];
+		StoreOp m_LastStores[512];
+
+		Memory* m_BootROM = nullptr;
 		Memory* m_Cartridge = nullptr;
-		RAM* m_VRAM = nullptr;
-		RAM* m_WRAM = nullptr;
+		Memory* m_VRAM = nullptr;
+		Memory* m_WRAM = nullptr;
 		CPU* m_CPU = nullptr;
 		PPU* m_PPU = nullptr;
 		Joypad* m_Joypad = nullptr;
 		uint8_t m_OAM[160] = {};
 		uint8_t m_HRAM[127] = {};
-		bool m_BootROMEnabled = true;
-
-		uint8_t m_TestCharacter;
 	};
 }
 
